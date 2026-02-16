8329567efcf6a6add605641cc5882bb6b933a857
Tanisha2210 <tanishabera123@gmail.com>
2026-02-16 16:52:50 +0530

    Update Hello.java for mini ci

--- Hello.java
+++ Hello.java
@@ -1,7 +1,5 @@
 class Hello {
     public static void main(String[] args){
-        System.out.println("Hello from GitHub!");
-        System.out.println("After configuring  the Poll SCM");
-
+        System.out.println("CI Working!");
     }
 }

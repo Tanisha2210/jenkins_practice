1facd5b291987db0b08d89b0e8aca78148336f23
Tanisha2210 <tanishabera123@gmail.com>
2026-02-16 13:12:56 +0530

    changes made

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
 class Hello {
     public static void main(String[] args){
         System.out.println("Hello from GitHub!");
-        System.out.println("After configuring Poll SCM");
+        System.out.println("After configuring  the Poll SCM");
 
     }
 }
